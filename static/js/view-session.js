48b7961fefefd77984a273da3cff8cf893608ef8
XD NIAN <xiaodong0601@qq.com>
2017-11-14 15:53:42 +0800

    view history updated

--- static/js/view-session.js
+++ static/js/view-session.js
@@ -2,7 +2,9 @@ var status_badge = {
     "Booked":'<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-info">Booked</span></h6>',
     "Committed": '<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-warning">Committed</span></h6>',
     "Finished": '<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-dark">Finished</span></h6>',
-    "Canceled": '<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-danger">Canceled</span></h6>'
+    "Canceled": '<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-danger">Canceled</span></h6>',
+    "Started": '<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">Started</span></h6>',
+    "Ended": '<h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">Ended</span></h6>'
 };
 
 $(document).ready(function(){
