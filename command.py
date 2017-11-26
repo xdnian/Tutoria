4bcf4a2d4f84ed87e406f9c47d66b49a6a6da98e
XD NIAN <xiaodong0601@qq.com>
2017-11-26 17:07:06 +0800

    [cron] command.py

--- command.py
+++ command.py
@@ -1,9 +1,29 @@
-import os
-import datetime
+import sys
+import runpy
+import datetime, time
 
-#os.system("python manage.py runserver &")
+# sys.argv = ['manage.py', 'runserver']
+# runpy.run_path('manage.py', run_name='__main__')
 
-while(1):
-    currentTime = datetime.datetime.now()
-    if (currentTime.minute == 30 or currentTime.minute == 0) and (currentTime.second == 0 and currentTime.microsecond == 0):
-        os.system("python manage.py all_sessions")
+try:
+    print ("Quit the session croning script with CTRL-BREAK.")
+    while True:
+
+        currentTime = datetime.datetime.now()
+        # print('[Debug]: current time - ' + currentTime)
+
+        if (currentTime.minute == 30 or currentTime.minute == 0) and currentTime.second == 0:
+            # print("[Debug]: cron")
+
+            # do cron
+            sys.argv = ['manage.py', 'all_sessions']
+            runpy.run_path('manage.py', run_name='__main__')
+            
+            # pause for 5 seconds (margin)
+            time.sleep(5)
+        
+        # loop every 0.5 second
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print('Session croning ended!')
